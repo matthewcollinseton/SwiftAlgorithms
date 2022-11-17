dc8e55d4d084570b79f3c942c0b1fff2d9bf86ee
Matthew Collins <m.collins@etoncollege.org.uk>
2022-11-17 18:01:58 +0000

    Initial Commit

---
+++ SwiftAlgorithms/SwiftAlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAlgorithmsApp.swift
+//  SwiftAlgorithms
+//
+//  Created by Collins, Matthew - MC on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
